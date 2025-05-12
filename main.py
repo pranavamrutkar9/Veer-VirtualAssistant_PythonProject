29c285a79984754db1de87a0af0a1573e7d5cbff
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-05-12 18:55:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi = "d7d2e6d04da744f6bb61f7b2abb13fb5"
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    print(c)
+    if "open google maps" in c.lower():
+        webbrowser.open("https://www.google.com/maps")
+    elif "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open chat gpt" in c.lower():
+        webbrowser.open("https://chatgpt.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=d7d2e6d04da744f6bb61f7b2abb13fb5")
+
+        if response.status_code == 200:
+            data = response.json()
+            articles = data.get('articles', [])
+            
+            print("Top Headlines:\n")
+            for idx, article in enumerate(articles[:10], start=1):  # Show top 10 headlines
+                print(f"{idx}. {article['title']}")
+                speak(f"{idx}. {article['title']}")
+        else:
+            print(f"Failed to fetch news: {response.status_code}")
+
+
+
+if __name__=="__main__":
+    speak("Initializing Veer......")
+    while True:
+        # listen for the wake word Veer
+        r = sr.Recognizer()
+        
+        try:
+            with sr.Microphone() as source:
+                print("Say something...")
+                audio = r.listen(source)
+            initial_call = r.recognize_google(audio)
+            print("You said:", initial_call)
+            if ("veer" in initial_call.lower()):
+                speak("Listening Sir")
+                # listen for command
+                with sr.Microphone() as source:
+                    print("Veer is listening...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    processCommand(command)
+
+        except sr.UnknownValueError:
+            print("Could not understand audio.")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "zinda" : "https://youtu.be/fP6MNznzVcQ?si=2nLZhnNeVWt0G3uk",
+    "talk" : "https://youtu.be/z8tol4skpK8?si=FxxLDQoC0TudsbKL",
+    "voices" : "https://youtu.be/-6AIqtlO23E?si=jZKl4iQ-nfjFbEUZ",
+    "crush" : "https://youtu.be/wOFp1lpZ6H8?si=fqVColDxj83vRm4y",
+    "orbit" : "https://youtu.be/KnSswhvEkbU?si=mSWMfjayr-Hf9f9c"
+}
